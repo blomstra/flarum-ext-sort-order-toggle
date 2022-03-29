737d2e530d4e1ef7d2eb7fd1587cfa91fa0e9b8f
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-03-30 00:51:12 +0200

    Only use URL search parameters for state management

--- js/src/forum/components/SortDirectionToggler.tsx
+++ js/src/forum/components/SortDirectionToggler.tsx
@@ -4,22 +4,48 @@ import app from 'flarum/forum/app';
 
 export class SortDirectionToggler extends Component {
   view() {
-    let sortDirection = (app as any).__sortDirection;
-
-    if (!['asc', 'desc'].includes(sortDirection)) sortDirection = 'desc';
+    const sortDirection = this.getSortDirection();
 
     return (
       <Button
         class="Button Button--icon Blomstra-SortDirectionToggle"
-        onclick={this.toggleSortDirection}
+        onclick={this.toggleSortDirection.bind(this)}
         aria-label={app.translator.trans(`blomstra-sort-order-toggle.forum.sort-toggle.${sortDirection}-label`)}
         icon={sortDirection === 'desc' ? 'fas fa-sort-amount-down' : 'fas fa-sort-amount-up'}
       />
     );
   }
 
+  protected getApiSort(): string {
+    const { sort } = app.search.params();
+
+    const sortMap = app.discussions.sortMap();
+
+    let apiSort = sortMap[sort || ''];
+
+    if (!apiSort) {
+      apiSort = Object.values(sortMap)[0];
+    }
+
+    return apiSort;
+  }
+
+  getSortDirection(): 'asc' | 'desc' {
+    return this.getApiSort().startsWith('-') ? 'desc' : 'asc';
+  }
+
   toggleSortDirection() {
-    (app as any).__sortDirection = (app as any).__sortDirection === 'asc' ? 'desc' : 'asc';
-    app.discussions.refresh();
+    const currentSort = this.getApiSort();
+
+    const newSort = currentSort.startsWith('-') ? currentSort.substring(1) : '-' + currentSort;
+
+    const sortMap = app.discussions.sortMap();
+
+    for (let sortParam in sortMap) {
+      if (sortMap[sortParam] === newSort) {
+        app.search.changeSort(sortParam);
+        break;
+      }
+    }
   }
 }
--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -3,7 +3,6 @@ import app from 'flarum/forum/app';
 
 import ItemList from 'flarum/common/utils/ItemList';
 import IndexPage from 'flarum/forum/components/IndexPage';
-import DiscussionListState from 'flarum/forum/states/DiscussionListState';
 
 import type Mithril from 'mithril';
 import { SortDirectionToggler } from './components/SortDirectionToggler';
@@ -15,20 +14,5 @@ app.initializers.add('blomstra/sort-order-toggle', () => {
     if ((app.current.data as any).routeName === 'byobuPrivate' || !items.has('sort')) return;
 
     items.add('sortDirectionToggle', <SortDirectionToggler />, 100);
-  });
-
-  extend(DiscussionListState.prototype, 'requestParams', function (this: DiscussionListState, params: Record<string, any>) {
-    if (!params) return;
-
-    // Enforce default
-    params.sort ||= Object.values(this.sortMap())[0];
-
-    if (params.sort) {
-      if (params.sort.startsWith('-') && (app as any).__sortDirection === 'asc') {
-        params.sort = params.sort.substr(1);
-      } else if (!params.sort.startsWith('-')) {
-        params.sort = '-' + params.sort;
-      }
-    }
   });
 });
